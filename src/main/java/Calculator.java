8788bc71d23b221e4345ed9d8d180e6c87b7a8d0
Anubhav Mohanty <xperience@Anubhavs-MacBook-Air.local>
2022-05-08 17:48:01 +0530

    commited

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,182 +1,72 @@
-
-
-import java.util.Scanner;
-import java.lang.Math;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Scanner;
+import java.io.*;
+
+
 public class Calculator {
-
-    public double square_Root(double num)
-    {
-        double output = 0;
-        try{
-            logger.info("Calculating Square root  of number " + num);
-            if(num < 0){
-                output = Double.NaN;
-                throw new ArithmeticException("Case of NaN squareRoot of < 0");
-            }
-            else{
-                output = Math.sqrt(num);
-            }
-        }catch(ArithmeticException err){
-            logger.error("Number cannot be negative " + err.getMessage());
-        }finally {
-            logger.info("Result of squareRoot is : " + output);
-        }
-
-        return output;
-    }
-
-    public double fact(double num)
-    {
-        double output = 1;
-        try{
-            if(num < 0) {
-                output = Double.NaN;
-                throw new ArithmeticException("Case of NaN factorial if < 0");
-            }
-            if(num!=(int)num)
-            {
-                output = Double.NaN;
-                throw new ArithmeticException("Case of NaN factorial if num is not an integer");
-            }
-
-            if(num == 0 || num == 1) return 1;
-
-            for(int i = 1; i <= num; i++){
-                output *= i;
-            }
-        }
-        catch(ArithmeticException err) {
-            logger.error("Number cannot be negative " + err.getMessage());
-        }
-        finally {
-            logger.info("Result of factorial is: " + output);
-        }
-
-        return output;
-    }
-
-    public double natural_Log(double num) {
-        double output = 0;
-        try{
-            logger.info("Calculating Natural log of " + num);
-            if(num <= 0){
-                output = Double.NaN;
-                throw new ArithmeticException("Case of NaN log of <= 0");
-            }
-            else{
-                output = Math.log(num);
-            }
-        }
-        catch(ArithmeticException err){
-            logger.error("Number cannot be negative " + err.getMessage());
-        }
-        finally {
-            logger.info("Result of naturalLog is : " + output);
-        }
-        return output;
-    }
-
-    public double power(double num1, double num2)
-    {
-        double output = 0;
-        if(num1==0.0&&num2==0.0)
-        {
-            output = Double.NaN;
-            logger.info("0 power 0 is not defined");
-            return output;
-        }
-        try {
-            logger.info("Calculating Power  of two numbers " + num1 + " and " + num2);
-            output = Math.pow(num1, num2);
-            if(output==Double.NaN) {
-                throw new ArithmeticException("Power is not real");
-            }
-        }catch (ArithmeticException ae)
-        {
-            logger.error(ae.getMessage());
-        }
-        logger.info("Result of power is : " + output);
-        return output;
-    }
-
     private static final Logger logger = LogManager.getLogger(Calculator.class);
+    public static void main(String[] args) throws IOException {
+        Calculations calculations = new Calculations();
 
-    public static void main(String[] args) {
-        double num1, num2;
-        Calculator calculator = new Calculator();
-        Scanner scanner = new Scanner(System.in);
-        char choice='y';
-        do {
+        Scanner sc = new Scanner(System.in);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
-            System.out.print("Enter an operator \n 1. sqrt \n 2. factorial \n 3. log \n 4. power ");
-            char operator = scanner.next().charAt(0);
-            //scanner.close();
-            double output=0.0;
-
-            switch (operator) {
-                case '1':
-                    System.out.print("Enter first number:");
-                    num1 = scanner.nextDouble();
-                    try {
-                        output = calculator.square_Root(num1)   ;
-                    }catch (Exception e)
-                    {
-                        System.out.println(e);
+        double result,num1,num2;
+        long factorial,fact_input;
+        char next = 'y';
+        while (next == 'y') {
+            System.out.println("----------Mini Calculator---------");
+            System.out.print("Enter what would you like to do\n1.Square Root\n2.Factorial\n3.Natural Logarithm\n4.Power Function\nEnter your choice: ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the number: ");
+                    num1 = sc.nextDouble();
+                    if (num1 < 0) {
+                        logger.error("Number entered to get square root is negative!");
+                    } else {
+                        result = calculations.squareRoot(num1);
+                        System.out.println("The square root is: " + Double.toString(result));
+                        logger.info("The square root of " + Double.toString(num1) + " is: " + Double.toString(result));
                     }
                     break;
-
-                case '2':
-                    System.out.print("Enter first number:");
-                    num1 = scanner.nextDouble();
-                    output = 1;
-                    try {
-                        output = calculator.fact(num1);
-                    }catch (Exception e)
-                    {
-                        System.out.println(e);
-                    }
-
-                    //output = num1;
-                    break;
-
-                case '3':
-                    System.out.print("Enter first number:");
-                    num1 = scanner.nextDouble();
-                    try {
-                        output = calculator.natural_Log(num1);
-                    }catch (Exception e)
-                    {
-                        System.out.println(e);
+                case 2:
+                    System.out.print("Enter the number: ");
+                    fact_input = sc.nextLong();
+                    if (fact_input < 0) {
+                        logger.error("Factorial of a negative number cannot be found!");
+                    } else {
+                        factorial = calculations.factorial(fact_input);
+                        System.out.println("The factorial is: " + Long.toString(factorial));
+                        logger.info("The factorial of" + Long.toString(fact_input) + " is: " + Long.toString(factorial));
                     }
                     break;
-
-                case '4':
-                    System.out.print("Enter first number:");
-                    num1 = scanner.nextDouble();
-                    System.out.print("Enter second number:");
-                    num2 = scanner.nextDouble();
-                    try {
-                        output = Math.pow(num1, num2);
-                    }catch(Exception e)
-                    {
-                        System.out.println(e);
+                case 3:
+                    System.out.print("Enter the number: ");
+                    num1 = sc.nextDouble();
+                    if (num1 < 0) {
+                        logger.error("Input to find Nautral Log is Negative!");
+                    } else {
+                        result = calculations.naturalLog(num1);
+                        System.out.println("The natural log is: " + Double.toString(result));
+                        logger.info("The natural log of" + Double.toString(num1) + " is: " + Double.toString(result));
                     }
-
                     break;
-
+                case 4:
+                    System.out.print("Enter the number: ");
+                    num1 = sc.nextDouble();
+                    System.out.print("Enter the power: ");
+                    num2 = sc.nextDouble();
+                    result = calculations.powerFunction(num1, num2);
+                    System.out.println("The power function value is: " + Double.toString(result));
+                    break;
                 default:
-                    System.out.printf("You have entered wrong operator");
-                    return;
+                    System.out.println("Inappropriate value chosen");
             }
-
-            System.out.println("Output: " + output);
-            System.out.println("Press y to continue");
-            choice = scanner.next().charAt(0);
-        }while(choice=='y');
-        scanner.close();
+            System.out.print("Enter a 'y' if you want to continue : ");
+            next = sc.next().charAt(0);
+        }
     }
 }
